ac04385e4ebd07529a8d924f072d178e2414ed09
wangxy0209 <74197548+wangxy0209@users.noreply.github.com>
2025-05-21 17:24:14 +0800

    Update 3Regmodel.js

--- 3Regmodel.js
+++ 3Regmodel.js
@@ -304,3 +304,9 @@ for (var i = 100; i < 110; i++) {
   // Adjust the range of tile indices as needed
   processAndExport(i);
 }
+
+/*
+Copyright (c) 2025 wangxy0209.
+This work is licensed under the terms of the MIT license.  
+For a copy, see https://opensource.org/licenses/MIT
+*/
